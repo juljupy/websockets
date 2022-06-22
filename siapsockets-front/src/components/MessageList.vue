593952bdbe496998277a013c299ae7183ef1f504
Julio de Hoyos <jhoyos@merqueo.com>
2022-06-22 09:54:14 -0500

    feat: Message form design

--- siapsockets-front/src/components/MessageList.vue
+++ siapsockets-front/src/components/MessageList.vue
@@ -3,23 +3,25 @@
     <h3 class="text-lg leading-6 font-medium text-gray-900 bg-gray-100 px-4 py-2">
       Messages from <span class="font-bold">{{ channel }}</span>
     </h3>
-    <div class="p-4">
-      <ul>
-        <li
-          v-for="(item, index) in messages"
-          :key="index"
-          class="border rounded-md shadow-sm m-2 p-4"
-        >
-          {{ item.message }}
-        </li>
-      </ul>
+    <div>
+      <div class="p-4">
+        <ul>
+          <li
+            v-for="(item, index) in messages"
+            :key="index"
+            class="border rounded-md shadow-sm m-2 p-4"
+          >
+            {{ item.message }}
+          </li>
+        </ul>
+      </div>
       <div
         v-if="channel !== ''"
-        class="flex space-x-2"
+        class="flex space-x-2 p-4 bg-gray-50 border-t"
       >
         <input
           v-model="message"
-          class="bg-gray-100 text-gray-400 rounded-md px-4 py-2 border border-gray-300 ring-0 flex-1"
+          class="text-gray-600 rounded-md px-4 py-2 border border-gray-300 ring-0 flex-1"
         >
         <button
           type="button"
